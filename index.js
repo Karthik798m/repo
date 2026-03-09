037572d6ccd6506e36dc5fda5dc9dd9f71d1f531
Karthik798m <karthikmanoj798m@gmail.com>
2026-03-09 22:47:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import nodemailer from "nodemailer"
-
+import pkg from "pg";
+const { Pool } = pkg;
 const app=express();
 const port=3000;
 
@@ -9,8 +10,20 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
+const db = new Pool({
+  user:"postgres",
+  host:"localhost",
+  database:"message",
+  password:"karthik123",
+  port:5432,
+
+});
+
+app.get("/",async(req,res)=>{
     res.render("home.ejs");
+  const result = await db.query("SELECT * FROM msg");
+  res.render("index", { users: result.rows });
+  console.log(result.rows);
 });
 
 app.get("/home",(req,res)=>{
@@ -26,20 +39,28 @@ app.get("/contact",(req,res)=>{
 });
 
 
-app.post("/submit",(req,res)=>{
+app.post("/submit",async(req,res)=>{
   var name=req.body["name"];
   var mail=req.body["email"];
   var cont=req.body["content"];
   console.log(name);
   console.log(mail);
   console.log(cont);
+
+  await db.query(
+    "INSERT INTO msg (name, mail, text) VALUES ($1, $2, $3)",
+  [name, mail, cont]
+  );
+
+ 
   
-   
+   res.render("contact.ejs");
+    
    const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: "karthiksecond96@gmail.com",          // acc details aan sukshikanam
-      pass: "vqrk zdxb qtmr kqjx"             
+      pass: "bmqw snmr ylxx hgsb"             
     }
    });
 
@@ -54,10 +75,10 @@ app.post("/submit",(req,res)=>{
 
 
  const mailOptions2 = {
-    from:  "karthiksecond96@gmail.com",  // sender (user’s email from form)
-    to: mail, // your inbox
+    from:  "smithakp4@gmail.com",  // sender (user’s email from form)
+    to: "karthikmanoj798m@gmail.com", // your inbox
     subject: "Thanks",
-    text: `Thank you for visiting my portfolio ${name} myra `
+    text: `Thank you for visiting my portfolio ${name} `
   };
 
 
@@ -66,9 +87,9 @@ app.post("/submit",(req,res)=>{
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
       console.error("Error sending email:", err);
-      res.send(" Error sending email.");
+      res.send("❌ Error sending email.");
     } else {
-      console.log(" Email sent:", info.response);
+      console.log("✅ Email sent:", info.response);
       
     }
   });
@@ -76,21 +97,20 @@ app.post("/submit",(req,res)=>{
   transporter.sendMail(mailOptions2, (err, info) => {
     if (err) {
       console.error("Error sending email:", err);
-      res.send(" Error sending email");
+      res.send("❌ Error sending email.");
     } else {
-      console.log(" Email sent:", info.response);
+      console.log("✅ Email sent:", info.response);
       
     }
   });
 
    
-res.render("contact.ejs");
+
 
    
 
 });
 
-app.listen(process.env.PORT||port,"0.0.0.0",()=>{
+app.listen(process.env.PORT||port,()=>{
   console.log("server is live on port "+port);
 });
-
